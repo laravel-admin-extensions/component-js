767c48b3aecc503ea9658dae3b052632d8dc82c4
hikki <hikki@qqc88.net>
2022-06-15 10:30:51 +0800

    v1 点线面

--- src/DLPViewer.php
+++ src/DLPViewer.php
@@ -166,78 +166,42 @@ EOF;
     }
 
     /**
-     * 新增表单-按钮
+     * 列-多操作添加 (旧版图标按钮模式)
      * @param Grid $grid
+     * @param array $settings [setting,...]
+     *  setting.document_class 自定义类名
+     *  setting.title 自定义按钮名 (图标css类 fa-edit fa-...)
+     *  setting.url 加载页地址
+     *  setting.xhr_url ajax提交地址
+     *  setting.method ajax提交方法
+     * @param array $disable ['view','edit','delete']
      */
-    public static function makeAddFormAction(Grid $grid)
+    public static function _makeRowPlaneAction(Grid $grid,array $settings = [
+        ['document_class'=>'','title'=>'','url'=>'','xhr_url'=>'','method'=>'POST']
+    ],array $disable=[])
     {
-        $url = Request::capture()->getPathInfo();
-        Admin::script(<<<EOF
-            $('.CAForm').click(function(){
-                componentPlane('{$url}/create','{$url}');
+        $script = '';
+        foreach ($settings as $setting){
+            $url = $setting['url'];
+            $method = isset($setting['method']) ? $setting['method'] : 'POST';
+            $xhr_url = isset($setting['xhr_url']) ? $setting['xhr_url'] : $url;
+            $script.=<<<EOF
+            $('.{$setting['document_class']}').click(function(){
+                let url = '$url'.replace('{id}',$(this).attr('data-id'));
+                componentPlane(url,'{$xhr_url}','{$method}');
             });
-EOF
-        );
-        $grid->disableCreateButton();
-        $grid->tools->append(new
-        class extends RowAction {
-            public function render()
-            {
-                return <<<EOF
-<div class="btn-group pull-right grid-create-btn" style="margin-right: 10px">
-    <a href='javascript:void(0);' class="btn btn-sm btn-success CAForm" title="新增">
-        <i class="fa fa-plus"></i><span class="hidden-xs">&nbsp;&nbsp;新增</span>
-    </a>
-</div>
 EOF;
+        }
+        Admin::script($script);
+        $grid->actions(function ($actions)use($settings,$disable) {
+            foreach ($settings as $setting) {
+                $actions->append("<a data-id='{$actions->getKey()}' href='javascript:void(0);' class='{$setting['document_class']}'><i class='fa {$setting['title']}'></i></a>");
             }
-        });
-    }
-
-    /**
-     * 修改表单-按钮
-     * @param Grid $grid
-     */
-    public static function makeEditFormAction(Grid $grid)
-    {
-        $grid->actions(function ($actions) {
-            $actions->disableEdit();
-            $url = Request::capture()->getPathInfo();
-            Admin::script(<<<EOF
-            $('.CEForm').click(function(){
-                let url = '{$url}' + '/'+this.getAttribute('data-id');
-                componentPlane(url+'/edit',url);
-            });
-EOF
-            );
-
-            $actions->add(new
-            class extends RowAction {
-                public function render()
-                {
-                    return "<a href='javascript:void(0);' class='CEForm' data-id='{$this->getKey()}'>修改</a>";
-                }
-            });
-        });
-    }
-
-    /**
-     * 修改表单-按钮  (旧版图标模式)
-     * @param Grid $grid
-     */
-    public static function _makeEditFormAction(Grid &$grid)
-    {
-        $grid->actions(function ($actions) {
-            $actions->disableEdit();
-            $url = Request::capture()->getPathInfo();
-            Admin::script(<<<EOF
-            $('.CEForm').click(function(){
-                let url = '{$url}' + '/'+this.getAttribute('data-id');
-                componentPlane(url+'/edit',url);
-            });
-EOF
-            );
-            $actions->append("<a data-id='{$actions->getKey()}' href='javascript:void(0);' class='CEForm'><i class='fa fa-edit'></i></a>");
+            foreach ($disable as $dis){
+                $dis == 'view' && $actions->disableView();
+                $dis == 'edit' && $actions->disableEdit();
+                $dis == 'delete' && $actions->disableDelete();
+            }
         });
     }
 
@@ -276,11 +240,10 @@ EOF
     /**
      * 表单代码段插入js片段代码
      * @param $script
-     * @return array|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public static function script($script)
     {
-        return Admin::script(<<<EOF
+        Admin::script(<<<EOF
 new Promise((resolve, reject) => {
     while (true){
         if(document.getElementById('component') instanceof HTMLElement){

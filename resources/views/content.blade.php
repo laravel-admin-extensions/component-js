b1cd5aeac310bcb8c52bb9ef47ebd020b49e7be1
ydtg1993 <946818508@qq.com>
2022-07-10 20:53:33 +0800

    1.8

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -1,8 +1,3 @@
-<style>
-    .modal-body::-webkit-scrollbar { width: 0 !important }
-    #component .row{margin: 0}
-    #component .row>div{padding: 0}
-</style>
 <div id="component">
    {!! $_content_ !!}
 </div>

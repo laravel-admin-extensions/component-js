d6391f37728dbf994d96341bafab4e2ce4f8880b
hikki <hikki@qqc88.net>
2022-08-24 15:36:32 +0800

    v3

--- src/Widget/CascadeDot.php
+++ src/Widget/CascadeDot.php
@@ -17,11 +17,12 @@ class CascadeDot extends DLPField
     {
         $id = $this->formatName($this->id);
         $height = isset($this->attributes['height']) ?  $this->attributes['height'] : '200px';
+        $limit = isset($this->attributes['limit']) ? (int)$this->attributes['limit'] : 0;
         $this->addVariables(['height'=>$height]);
         $select = json_encode($this->options, JSON_UNESCAPED_UNICODE | JSON_HEX_QUOT | JSON_HEX_APOS);
         $selected = json_encode($this->checked);
         $this->script = <<<EOT
-new ComponentCascadeDot("{$id}",JSON.parse('{$selected}'),JSON.parse('{$select}'));
+new ComponentCascadeDot("{$id}",JSON.parse('{$selected}'),JSON.parse('{$select}'),{$limit});
 EOT;
         return parent::render();
     }
--- src/Widget/CascadeLine.php
+++ src/Widget/CascadeLine.php
@@ -17,12 +17,11 @@ class CascadeLine extends DLPField
     {
         $id = $this->formatName($this->id);
         $height = isset($this->attributes['height']) ?  $this->attributes['height'] : '200px';
-        $limit = isset($this->attributes['limit']) ? (int)$this->attributes['limit'] : 0;
         $this->addVariables(['height'=>$height]);
         $select = json_encode($this->options, JSON_UNESCAPED_UNICODE | JSON_HEX_QUOT | JSON_HEX_APOS);
         $xhr = $this->xhr ?? '';
         $this->script = <<<EOT
-new ComponentCascadeLine("{$id}",JSON.parse('{$select}'),'{$xhr}',{$limit});
+new ComponentCascadeLine("{$id}",JSON.parse('{$select}'),'{$xhr}');
 EOT;
         return parent::render();
     }

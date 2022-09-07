50e3665ec38d54ae97dc03386c6e74d94b75da96
hikki <hikki@qqc88.net>
2022-09-07 13:36:36 +0800

    v3

--- resources/assets/component.js
+++ resources/assets/component.js
@@ -1,4 +1,4 @@
-const _component = {
+window._component = {
     'trash': `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
   <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -163,7 +163,7 @@ const _component = {
     }
 };
 
-class ComponentDot {
+window.ComponentDot = class {
     MODE = {
         insert: 'insert',
         delete: 'delete'
@@ -318,7 +318,7 @@ class ComponentDot {
     }
 }
 
-class ComponentCascadeDot {
+window.ComponentCascadeDot = class {
     MODE = {
         insert: 'insert',
         delete: 'delete'
@@ -711,7 +711,7 @@ class ComponentCascadeDot {
     }
 }
 
-class ComponentLine {
+window.ComponentLine = class {
     constructor(name, columns, data, options = {}) {
         this.DOM = document.getElementById(name);
         this.DOM.addEventListener("contextmenu", (e) => {
@@ -931,7 +931,7 @@ class ComponentLine {
     }
 }
 
-class ComponentPlane {
+window.ComponentPlane = class {
     constructor(url, xhr = {}, options = {}) {
         this.URL = url;
         this.XHR = Object.assign({
@@ -1025,7 +1025,7 @@ class ComponentPlane {
     }
 }
 
-class ComponentSortable {
+window.ComponentSortable = class {
     constructor(list, callback = null) {
         this.list = (typeof list === 'string') ? document.querySelector(list) : list;
         this.options = {
@@ -1155,7 +1155,7 @@ class ComponentSortable {
     }
 }
 
-class ComponentCascadeLine {
+window.ComponentCascadeLine = class {
     constructor(name, select, url) {
         if (!Array.isArray(select)) {
             console.error('CascadeLine param select must be array!');

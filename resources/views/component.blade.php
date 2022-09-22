34155ed28881007a25382ce44b288978ccccd686
hikki <hikki@qqc88.net>
2022-09-22 14:20:17 +0800

    v3.2

--- resources/views/component.blade.php
+++ resources/views/component.blade.php
@@ -1,8 +1,6 @@
-<div class="{{$viewClass['form-group']}} {!! !$errors->has($errorKey) ? '' : 'has-error' !!}">
+<div class="{{$viewClass['form-group']}}">
     <label for="{{$id}}" class="{{$viewClass['label']}} control-label">{{$label}}</label>
     <div class="{{$viewClass['field']}}">
-        @include('admin::form.error')
         <div id="{{$id}}" style="width: 100%; height: {{$height}}"></div>
-        @include('admin::form.help-block')
     </div>
 </div>

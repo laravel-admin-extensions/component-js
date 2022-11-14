4befbe8cff6edae70b0f3af35a3acf579c5c2efe
hikki <hikki@qqc88.net>
2022-11-14 11:35:32 +0800

    v3.6

--- test/example.php
+++ test/example.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use DLP\Tool\Assistant;
+use DLP\Tool\FormPanel;
 use DLP\Widget\Plane;
 use Encore\Admin\Admin;
 use Encore\Admin\Controllers\AdminController;
@@ -45,7 +46,7 @@ class ExampleController extends AdminController
          * url      弹窗页地址
          * xhr_url  表单提交地址
          * method   提交方式 POST PUT GET ...
-         * callback ajax请求回调js函数 (字符串方式书写)
+         * callback ajax请求回调函数
          *      function(response){
          *          alert(response);
          *      }
@@ -58,7 +59,7 @@ class ExampleController extends AdminController
          * url      弹窗页地址 {id}反向匹配当前行id
          * xhr_url  表单提交地址 {id}反向匹配当前行id
          * method   提交方式 POST PUT GET ...
-         * callback ajax请求回调js函数 (字符串方式书写)
+         * callback ajax请求回调函数
          *      function(response){
          *          alert(response);
          *      }
@@ -127,13 +128,9 @@ class ExampleController extends AdminController
 
         /**
          * 点组件
-         * options          设置数据集 一维数组 格式 [value1=>text1,value2=>text2...]
-         * checked          已选择 一维数组 值类型integer
-         * attribute.width  设置宽度 默认100%
+         * options 设置数据集 一维数组 格式 [value1=>text1,value2=>text2...]
+         * checked 已选择 一维数组 值类型integer
          * attribute.height 设置高度 默认200px
-         * attribute.limit  选择限制数 默认0:无限
-         * attribute.menu_mode  组件模式设置 false:默认模式 true:下拉列表模式
-         * attribute.menu_placeholder  下拉列表模式 默认未选择占位
          */
         $form->Dot('dot','标签选择器')
             ->options([1=>'松下紗栄子',2=>'上原亜衣',3=>'白石茉莉奈',4=>'美谷朱里',5=>'沖田杏梨',6=>'由愛可奈',7=>'七瀬あいり',8=>'五十嵐星蘭',9=>'仲里紗羽',10=>'波多野結衣'])
@@ -144,83 +141,93 @@ class ExampleController extends AdminController
          * 级联点组件
          * options 设置数据集 多维数组 格式[[key=>key1,val=>value1,nodes=>[...]],...]
          *          链表结构数据辅助组装
-         *              1.倒排父节点查询 注:id name parent_id 必须命别名 key val par
-         *          $select = Model::orderBy('par','DESC')->select('id as key','name as val','parent_id as par')->get()->toArray();
+         *              1.倒排父节点查询
+         *          $select = Model::orderBy('parent_id','DESC')->select('id as key','name as val','parent_id as par')->get()->toArray();
          *              2.辅助函数dimension 组装后的结构参考$this->cascadeData()的示例数据
          *          DLPHelper::dimension($select);
-         * checked          已选择 一维数组 值类型integer
-         * attribute.width  设置宽度 默认100%
+         * checked 已选择 一维数组 值类型integer
          * attribute.height 设置高度 默认200px
-         * attribute.limit  选择限制数 默认0:无限
          */
         $form->CascadeDot('cascadeDot','级联标签选择器')
-            ->options($this->cascadeExampleData())
+            ->options($this->cascadeData())
             ->checked([614,550,543])
             ->attribute(['height'=>'200px']);
 
         /**
          * 线组件
-         * columns[column...]       列数据格式配置
-         *  column.name             列表头名称
-         *  column.type             列数据 输出格式input,text,hidden,datetime,date,select,image,file
-         *  column.insert_type      增加列格式(不填时默认等同于type值) hidden表示置空
-         *  column.options          insert_type或type为select时 多选项
-         *  column.options_limit    insert_type或type为select时 多选项选择限制数 默认0:无限制
-         *  column.style            自定义style格式
+         * columns 设置列表head名称 row字段输出格式input,text,hidden
          * options 设置数据集 二维数组
-         * attribute.width   设置宽度 默认100%
-         * attribute.height  设置高度 默认355px
-         * attribute.options 设置操作列 默认开启:可排序/可删除/可新增['sortable' => true, 'delete' => true, 'insert' => true]
+         * attribute.height  设置高度 默认360px
+         * attribute.options 设置操作列 默认开启['sortable'=>true,'delete'=>true]
          */
         $form->Linear('flux_linkage', '磁力链接')
             ->columns([
                 'name' => ['name' => '名称', 'type' => 'input'],
-                'meta' => ['name' => '信息', 'type' => 'input','insert_type'=>'hidden'],
-                'url' => ['name' => '链接', 'type' => 'image'],
-                'time' => ['name' => '更新时间', 'type' => 'text','insert_type'=>'datetime'],
-                'is-small' => ['name' => '高清', 'type' => 'select','options'=>[1=>'是',2=>'否'],'options_limit'=>1, 'style' => 'width:60px']
+                'meta' => ['name' => '信息', 'type' => 'input'],
+                'url' => ['name' => '链接', 'type' => 'input'],
+                'time' => ['name' => '更新时间', 'type' => 'text'],
+                'is-small' => ['name' => '高清[1是 2否]', 'type' => 'input', 'style' => 'width:60px'],
+                'is-warning' => ['name' => '含字幕[1是 2否]', 'type' => 'input', 'style' => 'width:60px']
             ])
             ->options([
-                ['name'=>'01','meta'=>'test info1','url'=>'1','time'=>'2021-05-15 00:00:00','is-small'=>1],
-                ['name'=>'02','meta'=>'test info2','url'=>'2','time'=>'2021-05-15 00:00:00','is-small'=>1],
-                ['name'=>'03','meta'=>'test info3','url'=>'3','time'=>'2021-05-15 00:00:00','is-small'=>1]])
-            ->attribute(['height' => '360px','options'=>['sortable' => true, 'delete' => true, 'insert' => true]]);
+                ['name'=>'01','meta'=>'test info1','url'=>'1','time'=>'2021-05-15 00:00:00','is-small'=>1,'is-warning'=>1],
+                ['name'=>'02','meta'=>'test info2','url'=>'2','time'=>'2021-05-15 00:00:00','is-small'=>1,'is-warning'=>1],
+                ['name'=>'03','meta'=>'test info3','url'=>'3','time'=>'2021-05-15 00:00:00','is-small'=>1,'is-warning'=>1]]);
+
         /**
          * 级联线组件
          * options 设置数据集 多维数组 格式[[key=>key1,val=>value1,nodes=>[...]],...]
          *          链表结构数据辅助组装
-         *              1.倒排父节点查询 注:id name parent_id 必须命别名 key val par
+         *              1.倒排父节点查询
          *          $select = Model::orderBy('parent_id','DESC')->select('id as key','name as val','parent_id as par')->get()->toArray();
          *              2.辅助函数dimension 组装后的$select结构参考$this->cascadeExampleData()返回数据
          *          DLPHelper::dimension($select);
-         * xhr 接口地址 编码参见文件:test\CascadeLineController 路由配置$router->resource('xhr地址', 'CascadeLineController')
-         * attribute.width   设置宽度 默认100%
-         * attribute.height  设置高度 默认200px
-         * attribute.options 设置 可迁移(迁移该节点与其子集到其他节点下)/可交换(节点与其后代节点交换位置)/可新增/可修改/可删除
-         *           ['movable' => true,'exchange' => true,'insert' => true,'update' => true,'delete' => true]
+         * xhr 接口地址 编码参见:test\CascadeLineController
+         * attribute.height 设置高度 默认200px
          */
         $form->CascadeLine('cascadeLine','级联标签管理器')
             ->options($this->cascadeExampleData())
-            ->xhr('.../xhr地址')
-            ->attribute(['height' => '200px','options'=>['movable' => true,'exchange' => true,'insert' => true,'update' => true,'delete' => true]]);
+            ->xhr('route-to-CascadeLineController');
         return $form;
     }
 
     public function blank()
     {
-        $html = '<h1>弹窗模式 自定页</h1>';
+        $title = '<h1>松下紗栄子</h1>';
         /*弹窗模式 渲染自定义页模板 Plane::html*/
         $panel = new FormPanel();
         $panel->input('id','序号');
-        $panel->textarea('description','描述');
+        $panel->textarea('descript','描述');
         $panel->select('status','状态',[0],[0=>'开启',1=>'关闭',2=>'删除'],1);
         $panel->datepicker('time','时间');
         $panel->html('test','自定义','<p>松下紗栄子</p>');
+        /*多图/文件 上传样例*/
+        $settings = [
+            'uploadUrl' => 'https://...upload.file.url...',
+            'uploadExtraData' => [
+                '_token' => csrf_token(),
+                'uploadAsync' => true,
+                /*自定义加传参*/
+            ],
+            'deleteUrl' => 'https://...delete.file.url...',
+            'deleteExtraData' => [
+                '_token' => csrf_token(),
+                'uploadAsync' => true,
+                /*自定义加传参*/
+            ],
+            'maxFileCount' => 10,
+            'maxFileSize' => 800 //单图限制800kb
+        ];
+        $images = ['/image1...','/image2...','/image3...'];
+        $panel->fileInput('photo','艳照',$settings,
+            ['files' => $images, 'url' => '/image.server...'],
+            "accept='image/*'");
+        
         $html = $panel->compile();
         return Plane::html($title.$html);
     }
 
-    private function cascadeExampleData()
+    private function cascadeData()
     {
         return [
             ["key" => "3", "val" => "基本", "nodes" => [

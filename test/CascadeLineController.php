09c8212fd4e1495f035a15ef76444e57da85e602
hikki <hikki@qqc88.net>
2022-11-21 12:11:19 +0800

    v4.0

--- test/CascadeLineController.php
+++ test/CascadeLineController.php
@@ -12,12 +12,18 @@ class CascadeLineController
      */
     use CascadeLineTrait;
 
+    public function show($id, Content $content)
+    {
+        // 节点详情查看页 例子.
+        return "详情查看页";
+    }
+
     /**
      * @inheritDoc
      */
     function createForm(FormPanel $formPanel)
     {
-        // TODO: CODE
+        // 节点新增 表单列设置 例子.
         $formPanel->input('insert_node_val','新增节点 name');
     }
 
@@ -29,7 +35,7 @@ class CascadeLineController
         $request = Request::capture();
         $key = (int)$request->input('key');
         $insert_node_name = $request->input('insert_node_val');
-        // TODO: CODE
+        // 节点新增逻辑处理
         // 新增id 随机示例
         $insert_node_id = $key+mt_rand(10000,100000);
         return ['key'=>$insert_node_id,'val'=>$insert_node_name];
@@ -42,7 +48,7 @@ class CascadeLineController
     {
         $request = Request::capture();
         $val = $request->input('val');
-        // TODO: CODE
+        // 节点修改 表单列设置 例子.
         $formPanel->input('update_node_name','修改节点 name',$val);
     }
 
@@ -53,7 +59,7 @@ class CascadeLineController
     {
         $request = Request::capture();
         $val = $request->input('update_node_name');
-        // TODO: CODE
+        // 节点修改逻辑处理
         return ['val'=>$val];
     }
 
@@ -62,6 +68,15 @@ class CascadeLineController
      */
     function destroyAction($id)
     {
-        // TODO: Implement destroyAction() method.
+        // 节点删除逻辑处理
+    }
+
+    /**
+    * @inheritDoc
+    */
+    function migrate()
+    {
+       // 节点迁移 交换 逻辑处理   
+       return Assistant::result(true);
     }
 }

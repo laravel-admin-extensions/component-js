a8fa0fc56195330073a6c7b40e6886dc3db3abf1
hikki <hikki@qqc88.net>
2023-01-05 10:10:09 +0800

    v4.7

--- src/DLPServiceProvider.php
+++ src/DLPServiceProvider.php
@@ -29,8 +29,8 @@ class DLPServiceProvider extends ServiceProvider
         }
 
         Admin::booting(function () {
-            Admin::css('vendor/dlp/component.min.css?v4.6');
-            Admin::headerJs('vendor/dlp/component.min.js?v4.6');
+            Admin::css('vendor/dlp/component.min.css?v4.7');
+            Admin::headerJs('vendor/dlp/component.min.js?v4.7');
             Form::extend('Dot', Dot::class);
             Form::extend('CascadeDot', CascadeDot::class);
             Form::extend('Linear', Linear::class);

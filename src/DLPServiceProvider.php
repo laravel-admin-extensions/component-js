51cf01251c647cbc45bf47bd12ed6125a9b232df
ydtg1993 <946818508@qq.com>
2023-03-07 20:31:27 +0800

    6.9.5

--- src/DLPServiceProvider.php
+++ src/DLPServiceProvider.php
@@ -29,8 +29,8 @@ class DLPServiceProvider extends ServiceProvider
         }
 
         Admin::booting(function () {
-            Admin::css('vendor/dlp/component.min.css?v6.9.3');
-            Admin::headerJs('vendor/dlp/component.min.js?v6.9.3');
+            Admin::css('vendor/dlp/component.min.css?v6.9.5');
+            Admin::headerJs('vendor/dlp/component.min.js?v6.9.5');
         });
     }
 }

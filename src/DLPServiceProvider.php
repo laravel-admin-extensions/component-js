d3caeca63641f451206963957195371e967e0f20
hikki <hikki@qqc88.net>
2023-03-08 14:10:32 +0800

    7

--- src/DLPServiceProvider.php
+++ src/DLPServiceProvider.php
@@ -29,8 +29,8 @@ class DLPServiceProvider extends ServiceProvider
         }
 
         Admin::booting(function () {
-            Admin::css('vendor/dlp/component.min.css?v6.9.6');
-            Admin::headerJs('vendor/dlp/component.min.js?v6.9.6');
+            Admin::css('vendor/dlp/component.min.css?v7.0');
+            Admin::headerJs('vendor/dlp/component.min.js?v7.0');
         });
     }
 }
